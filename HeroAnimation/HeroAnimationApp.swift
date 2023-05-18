eff4b9e8a87b04a4ddca5064d11d768023771033
Komal Gilani <komalsyeda29@gmail.com>
2023-05-18 21:27:05 +0500

    Initial Commit

---
+++ HeroAnimation/HeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroAnimationApp.swift
+//  HeroAnimation
+//
+//  Created by Komal Gilani on 18/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct HeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
