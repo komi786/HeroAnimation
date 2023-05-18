789d33c72d97b0fda92bf6d3e0e9141f8ba64cc5
Komal Gilani <komalsyeda29@gmail.com>
2023-05-18 23:24:10 +0500

    first commit

--- HeroAnimation/ContentView.swift
+++ HeroAnimation/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var images = [
+     "aidkit",
+     "doublebed",
+     "bandaid",
+     "drugs",
+     "heart",
+     "xmark"
+    ]
+    @State private var selectedimages  : [String] = []
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("All Images")
+                .font(.headline)
+            allImagesView
+            
+            Spacer()
+            Text("Selected Images")
+                .font(.headline)
+            selectedImagesView
+         
+           
         }
         .padding()
+    }
+
+    private var allImagesView : some View {
+    LazyVGrid(columns: [.init(.adaptive(minimum: 88))]){
+        
+        ForEach(images, id:\.self) {
+            image in
+            
+            Image(image)
+                .resizable()
+                .frame(width: 44,height: 44)
+                .onTapGesture{
+                    withAnimation {
+                        images.removeAll{$0 ==  image}
+                        selectedimages.append(image)
+                    }
+                }
+            
+        }
+       }
+    }
+    
+    private var selectedImagesView : some View {
+    LazyVGrid(columns: [.init(.adaptive(minimum: 88))]){
+        
+        ForEach(selectedimages, id:\.self) {
+            image in
+            
+            Image(image)
+                .resizable()
+                .frame(width: 44,height: 44)
+                .onTapGesture{
+                    withAnimation {
+                        selectedimages.removeAll{$0 ==  image}
+                        images.append(image)
+                    }
+                }
+            
+        }
+       }
     }
 }
 
--- HeroAnimation/HeroAnimationApp.swift
+++ HeroAnimation/HeroAnimationApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct HeroAnimationApp: App {
+    
+   
     var body: some Scene {
         WindowGroup {
             ContentView()
